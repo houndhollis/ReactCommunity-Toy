349094d3560d6c7a061a16ea911f591b9707442c
houndhollis <houndhollis9797@gmail.com>
2022-10-27 01:51:36 +0900

    make it better

--- App/client/src/components/InfoList.js
+++ App/client/src/components/InfoList.js
@@ -12,7 +12,7 @@ const InfoList = () => {
                 <Avatar size='40' round={true} src={`${process.env.PUBLIC_URL}/assets/admin.png`}/>
                 <div className='userInfo'>
                   <p className='author'>어드민</p>
-                  <p className='time'>2022년 10월 27일, 01:32 (고정글)</p>
+                  <p className='time'>2022년 10월 27일, 01:32 (고정글!)</p>
                 </div>
               </div>
       <p className='postContent'>프로필 사진, 글 작성 이미지 사진 오류 해결 예정 네이버 클라우드 사용</p>

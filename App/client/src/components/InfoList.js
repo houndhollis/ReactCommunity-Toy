a18b39992fd578b8a74aae767537397e23c64f68
houndhollis <houndhollis9797@gmail.com>
2022-12-14 14:15:21 +0900

    feat:git connect2

--- App/client/src/components/InfoList.js
+++ App/client/src/components/InfoList.js
@@ -12,7 +12,7 @@ const InfoList = () => {
                 <Avatar size='40' round={true} src={`${process.env.PUBLIC_URL}/assets/admin.png`}/>
                 <div className='userInfo'>
                   <p className='author'>어드민</p>
-                  <p className='time'>2022년 10월 27일, 01:32 (고정글!)</p>
+                  <p className='time'>2022년 12월 14일, 01:32 (고정글!)</p>
                 </div>
               </div>
       <p className='postContent'>글 작성 이미지 사진 에러 수정, favicon 수정</p>
